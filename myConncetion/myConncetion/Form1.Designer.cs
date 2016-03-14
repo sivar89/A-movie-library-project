23b5c9d614eb446c9d515a2c30fd65f87feafa17
Sivar Mirza <sivarmirza@hotmail.se>
2016-03-14 11:42:50 +0100

    Uploaded files

---
+++ myConncetion/myConncetion/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace myConncetion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.genre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stars = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.director = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.description);
+            this.groupBox1.Controls.Add(this.genre);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.stars);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.director);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.name);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(421, 563);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Movies";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Add movie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(29, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Description";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(32, 353);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(350, 159);
+            this.description.TabIndex = 8;
+            // 
+            // genre
+            // 
+            this.genre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genre.Location = new System.Drawing.Point(32, 280);
+            this.genre.Name = "genre";
+            this.genre.Size = new System.Drawing.Size(215, 23);
+            this.genre.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Genre";
+            // 
+            // stars
+            // 
+            this.stars.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stars.Location = new System.Drawing.Point(32, 203);
+            this.stars.Name = "stars";
+            this.stars.Size = new System.Drawing.Size(215, 23);
+            this.stars.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Stars";
+            // 
+            // director
+            // 
+            this.director.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.director.Location = new System.Drawing.Point(32, 133);
+            this.director.Name = "director";
+            this.director.Size = new System.Drawing.Size(215, 23);
+            this.director.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Director";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name of the movie";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(32, 68);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(215, 23);
+            this.name.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(451, 20);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(723, 555);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Retrie movie";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 195);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(480, 343);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(57, 217);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(48, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 25);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Information";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Find";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(53, 60);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(244, 23);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(50, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 17);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Search ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(516, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "List of movies";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(519, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(170, 446);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(614, 515);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1186, 605);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox director;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox stars;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox genre;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ myConncetion/myConncetion/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace myConncetion
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection MyConn3;
+        MySqlCommand cmd;
+        static string MyConnection3 = "datasource=localhost; database=movie;port=3306;username=root;password=666666";
+        Color myRgbColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.BackColor = Color.FromArgb(224, 224, 224);
+
+            fill_listbox();
+        }
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.name.Text) ||
+                    string.IsNullOrWhiteSpace(this.director.Text) ||
+                    string.IsNullOrWhiteSpace(this.stars.Text) ||
+                        string.IsNullOrWhiteSpace(this.genre.Text) ||
+                    string.IsNullOrWhiteSpace(this.description.Text))
+            {
+                MessageBox.Show("Please dont leave any field empty");
+            }
+            else {
+                try
+                {
+                    //This is my insert query in which i am taking input from the user through windows forms
+                    string Query = "insert into movie.new_table(name,director,stars,genre,description) values('" + this.name.Text + "','" + this.director.Text + "','" + this.stars.Text + "','" + this.genre.Text + "','" + this.description.Text + "');";
+
+                    //This is  MySqlConnection here i have created the object and pass my connection string.
+                    MyConn3 = new MySqlConnection(MyConnection3);
+
+                    //This is command class which will handle the query and connection object.
+                    cmd = new MySqlCommand(Query, MyConn3);
+                    MySqlDataReader MyReader2;
+                    MyConn3.Open();
+                    MyReader2 = cmd.ExecuteReader();     // Here our query will be executed and data saved into the database.
+
+                    MessageBox.Show("Movie added");
+                }
+                catch (Exception ex)
+
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                MyConn3.Close();
+            }
+            last_record();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myRgbColor = new Color();
+            myRgbColor = Color.FromArgb(255, 153, 51);
+
+            MyConn3 = new MySqlConnection(MyConnection3);
+            if (string.IsNullOrWhiteSpace(this.textBox6.Text))
+            {
+                MessageBox.Show("Please enter a name");
+            }
+            else {
+                string test1 = "select count(*) from new_table where name='" + this.textBox6.Text + "'";
+                cmd = new MySqlCommand(test1, MyConn3);
+                try
+                {
+                    MyConn3.Open();
+                    Int32 count = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    if (count == 0)
+                    {
+                        MessageBox.Show("Movie not found");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            MyConn3.Close();
+
+            string query = ("select director, name, stars, genre, description from new_table where name='" + this.textBox6.Text + "'");
+            getData(query);
+        }
+
+        public void fill_listbox()
+        {
+            MyConn3 = new MySqlConnection(MyConnection3);
+            string test = ("select name from new_table");
+            cmd = new MySqlCommand(test, MyConn3);
+
+            MySqlDataReader MyReader2;
+            try
+            {
+                MyConn3.Open();
+                MyReader2 = cmd.ExecuteReader();
+                while (MyReader2.Read())
+                {
+                    string eName = MyReader2.GetString("name");
+                    listBox1.Items.Add(eName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MyConn3.Close();
+        }
+        public void last_record()
+        {
+            MyConn3 = new MySqlConnection(MyConnection3);
+            string test = ("SELECT name FROM new_table order by id desc limit 1");
+            cmd = new MySqlCommand(test, MyConn3);
+
+            try
+            {
+                MyConn3.Open();
+                string meep = (string)cmd.ExecuteScalar();
+
+                listBox1.Items.Add(meep);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MyConn3.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string query = "select director, name, stars, genre, description from new_table where name='" + listBox1.SelectedItem.ToString() + "'";
+            getData(query);
+        }
+        public void style_info()
+        {
+            myRgbColor = new Color();
+            myRgbColor = Color.FromArgb(255, 153, 51);
+            string[] col = { "Name:", "Director:", "Stars:", "Genre:", "Description:" };
+
+            for (int i = 0; i < col.Length; i++)
+            {
+                richTextBox1.Select(richTextBox1.Text.IndexOf(col[i]), col[i].Length);
+                richTextBox1.SelectionFont = new Font("Lucida Handwriting", 12, FontStyle.Bold);
+                richTextBox1.SelectionColor = myRgbColor;
+            }
+        }
+
+        public void getData(string query)
+        {
+            cmd = new MySqlCommand(query, MyConn3);
+            MySqlDataReader MyReader2;
+            try
+            {
+                MyConn3.Open();
+                MyReader2 = cmd.ExecuteReader();
+                while (MyReader2.Read())
+                {
+                    for (int i = 0; i < MyReader2.FieldCount; i++)
+                    {
+                        richTextBox1.ReadOnly = true;
+                        string name = " Name: ";
+                        string director = " Director: ";
+                        string stars = " Stars: ";
+                        string genre = " Genre: ";
+                        string description = " Description: ";
+
+                        richTextBox1.Text = name + Environment.NewLine + Environment.NewLine + MyReader2["name"].ToString() + "    "
+                            + Environment.NewLine + Environment.NewLine
+                            + director
+                            + Environment.NewLine + Environment.NewLine
+                            + MyReader2["director"].ToString() + "    "
+
+                             + Environment.NewLine + Environment.NewLine
+                            + stars
+                             + Environment.NewLine + Environment.NewLine
+                            + MyReader2["stars"].ToString() + "    "
+
+                             + Environment.NewLine + Environment.NewLine
+                             + genre
+                             + Environment.NewLine + Environment.NewLine
+                             + MyReader2["genre"].ToString() + "    "
+
+                             + Environment.NewLine + Environment.NewLine
+                             + description
+                             + Environment.NewLine + Environment.NewLine
+                             + MyReader2["description"].ToString() + "    ";
+
+                        style_info();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MyConn3.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {           
+            try
+            {
+                string query = "delete from new_table where name='" + listBox1.SelectedItem+ "'";
+                MyConn3 = new MySqlConnection(MyConnection3);
+                MySqlCommand MyCommand2 = new MySqlCommand(query, MyConn3);
+                MySqlDataReader MyReader2;
+                MyConn3.Open();
+                MyReader2 = MyCommand2.ExecuteReader();
+                MessageBox.Show("Movie Deleted");
+
+                listBox1.Items.Clear();
+                fill_listbox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            MyConn3.Close();           
+        }
+       
+    }
+
+}
